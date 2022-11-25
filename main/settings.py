679e90a1779d3ea330a44d505ce6ad852b21ff44
joshatre <joshatre@gmail.com>
2022-11-25 00:27:09 -0800

    Added drop down menu for events subpage

--- main/settings.py
+++ main/settings.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -132,3 +137,10 @@ MEDIA_URL = '/media/'
 STATICFILES_DIRS = [
     BASE_DIR / 'main/static/',
 ]
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'SCU eSports <maxmac3731@gmail.com>'
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
