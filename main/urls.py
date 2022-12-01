f362e05b2b54fa432bb740558341272e32fa4437
joshatre <joshatre@gmail.com>
2022-11-30 23:58:29 -0800

    Improved UI for login page

--- main/urls.py
+++ main/urls.py
@@ -32,4 +32,4 @@ urlpatterns = [
     path('accounts/', include('accounts.urls'))
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
